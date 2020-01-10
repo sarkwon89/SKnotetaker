cce319418aa44644e1b7d9013e411ff32bc164cb
Sarah <sarkwon89@gmail.com>
2020-01-09 16:43:14 -0800

    created the delete endpoint

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ let util = require("util");
 let app = express();
 
 const writeFileAsync = util.promisify(fs.writeFile);
-const appendFileAsync = util.promisify(fs.appendFile);
 const readFileAsync = util.promisify(fs.readFile);
 
 //In order to make sure your server works on local & heroku you have to make sure the commandline looks like this
@@ -38,7 +37,7 @@ app.get('/notes', (req, res) => {
 app.get("/api/notes", function (req, res) {
     readFileAsync("./db/db.json", "utf8").then(function (data) {
         data = JSON.parse(data)
-        console.log(data)
+        // console.log(data)
         return res.json(data);
     })
 });
@@ -47,39 +46,50 @@ app.get("/api/notes", function (req, res) {
 app.post("/api/notes", function (req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
+    // create a variable and dump the data you got from the post ajax call in here
     var newNotes = req.body;
 
-    console.log(newNotes)
+    // console.log(newNotes)
 
     //read the db.json file to grab the arrays of object and return json
     readFileAsync("./db/db.json", "utf8").then(function (data) {
         data = JSON.parse(data)
+        // console.log(data);
+        //  push the new data into the db.json
+        data.push(newNotes);
 
-        data.push(newNotes)
-
-        // data.push(data[data.length - 1].id=data.length-1)
-        //    push the new data into the db.json
-       
-        
-        console.log(data.length)
-        console.log(data[data.length - 1].id=data.length-1)
-        console.log(data)
+        // data.push(data[data.length - 1].id=data.length-1) is grabbling the length and appending to the object
+        data[data.length - 1].id=data.length-1;
 
         //once the new note is added to the array from db.json file then write the upated changes
         writeFileAsync("./db/db.json", JSON.stringify(data));
-
     })
     res.send("created notes!")
 })
 
-//Create a function that will increment an integer when a new note is received.  Then apply the array of integers to your array of notes
-
-
-
 
 // DELETE `/api/notes/:id` - Should receive a query paramter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 app.delete("/api/notes/:id", function (req, res) {
-    // This works because of our body parsing middleware
+    // This works because of our body parsing 
+    var idReceived = req.params.id
+    console.log(idReceived)
+    
+    //read the db.json file to grab the arrays of object and return json
+    readFileAsync("./db/db.json", "utf8").then(function (data) {
+        //turn object into string
+        data = JSON.parse(data)
+        
+        //array splice will remove the position I ask it
+        data.splice(idReceived,1);
+        
+        //the for loop to reset the index of the array
+        for (let i = 0; i < data.length; i++) {
+            data[i].id=i;
+        };
+
+        //once the new note is added to the array from db.json file then write the upated changes
+        writeFileAsync("./db/db.json", JSON.stringify(data));
+    })
     res.send("cleared!")
 });
 
